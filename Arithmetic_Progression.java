24c44c0554dc2fd246960586cb26fbe680e1c73e
Alex Programmer <63038222+alexprogrammer07@users.noreply.github.com>
2022-10-01 15:39:59 +0530

    Improved Readability

--- Arithmetic_Progression.java
+++ Arithmetic_Progression.java
@@ -1,21 +1,19 @@
-package com.akshat.number_programs;
-
-
 import java.io.*;
 import java.lang.*;
 
 public class Arithmetic_Progression {
     public static void main(String[] args)throws IOException {
 
-        InputStreamReader isr = new InputStreamReader(System.in);
+        InputStreamReader isr = new InputStreamReader(System.in);  // Create object
         BufferedReader br = new BufferedReader(isr);
         BufferedReader br2 = new BufferedReader(isr);//br2 is used because while executing the program, error of "empty string" is shown by the java compiler
 
         System.out.println("To Calculate Difference terms of Arithmetic Progression you choices are :\n First term = a \n Last term = l \n Number of terms = n \n Common Difference = d");
-        //Showing the choices to the user
+        // Showing the choices to the user
         System.out.println("\nEnter your choice");
         char ch = (char) br2.read();
-
+        
+        // Declare variables
         double ans = 0.0;
         int flag = 0;
 
@@ -71,7 +69,8 @@ public class Arithmetic_Progression {
             System.out.println("Invalid Choice : Please Enter a valid choice");
             flag = 1;//to prevent double output flag is used
         }
-        if (flag==0) {
+        if (flag==0) // Condition to get the final output
+        {
             System.out.println("Your Answer is "+(int)ans);//showing output in int data type
         }
 
